1c593cc15f1a2e2c1ff2ac18570ca33eb7b20a2f
Marcin Tatoń <marcin.taton@simpleformat.pl>
2023-07-25 10:12:50 +0200

    experimenting with Catalyst patterns

--- src/NLSE/NLP.cs
+++ src/NLSE/NLP.cs
@@ -21,7 +21,7 @@ namespace NLSearchWeb.src.NLSE
         {
             /// Process the text
 
-            // not very reliable on short texts which is a concern
+            // language detection not very reliable on short texts which is a concern - causes 500 by the way
             var cld2LanguageDetector = await LanguageDetector.FromStoreAsync(Language.Any, Mosaik.Core.Version.Latest, "");
             var doc = new Document(input);
             cld2LanguageDetector.Process(doc);
@@ -30,34 +30,40 @@ namespace NLSearchWeb.src.NLSE
 
             Console.WriteLine("Language: " + doc.Language);
             var nlp = Pipeline.For(doc.Language);
-            // nlp.Add(await AveragePerceptronEntityRecognizer.FromStoreAsync(language: doc.Language, version: Mosaik.Core.Version.Latest, tag: "WikiNER"));
 
+            // -----------------------------------------------------------------
+            // current algorithm to pull interesting words and phrases, using Catalyst patterns
+            // perhaps should be replaced by custom algorithm to pull all n-grams manually - which would produce a lot of garbage, 
+            // but pre model filtering would take care of that
             var singlePattern = new PatternSpotter(doc.Language, 0, tag: "singlePattern", captureTag: "singlePattern");
             singlePattern.NewPattern(
                 "singlePattern",
                 mp => mp.Add(
-                    new PatternUnit(P.SingleOptional().WithPOS(PartOfSpeech.NOUN, PartOfSpeech.PROPN, PartOfSpeech.ADJ, PartOfSpeech.NUM, PartOfSpeech.X))
+                    new PatternUnit(P.Single().WithPOS(PartOfSpeech.NOUN, PartOfSpeech.PROPN, PartOfSpeech.ADJ, PartOfSpeech.NUM, PartOfSpeech.X))
             ));
             nlp.Add(singlePattern);
 
+            // especially here, by adding or removing ADJ from multiPattern
+            // it either collects garbage or skips important stuff
+            // as it is if a 3-gram covers a phrase, 2-grams from that phrase will not be generated 
+            // so... far from perfect
             var multiPattern = new PatternSpotter(doc.Language, 0, tag: "multiPattern", captureTag: "multiPattern");
             multiPattern.NewPattern(
                 "multiPattern",
                 mp => mp.Add(
-                    new PatternUnit(P.MultipleOptional().WithPOS(PartOfSpeech.NOUN, PartOfSpeech.PROPN, PartOfSpeech.NUM, PartOfSpeech.X))
+                    new PatternUnit(P.Multiple().WithPOS(PartOfSpeech.NOUN, PartOfSpeech.PROPN, PartOfSpeech.NUM, PartOfSpeech.X))
             ));
             nlp.Add(multiPattern);
+            // -----------------------------------------------------------------
+
 
             nlp.ProcessSingle(doc);
 
             /// Pull the good shit out
-
-
             var poiValues = new List<string>();
 
             var entities = doc.SelectMany(span => span.GetEntities());
 
-            // Pass response
             foreach (var token in entities)
             {
                 poiValues.Add(token.Value);
